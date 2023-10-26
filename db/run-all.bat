a6b3ae1d3044597976e759f0770d3028eeed4a73
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-10-26 13:12:20 +1000

    minor commit

--- db/run-all.bat
+++ db/run-all.bat
@@ -3,3 +3,4 @@ sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -E -i Setup.sql
 sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -E -i Insert.sql
 sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -E -i Query.sql
 sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -E -i Drop.sql
+pause
